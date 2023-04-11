a10f109215292a431b5f1f43733b1d7ed0ceb622
jorgecif <jorgecif@gmail.com>
2023-04-10 20:33:52 -0500

    Update

--- app.py
+++ app.py
@@ -2,54 +2,53 @@ import streamlit as st
 import PIL.Image
 import requests
 import random
+import spacy
+from streamlit_option_menu import option_menu
 
 
+# Variables de sesión
 
-def consultar():
+if 'puntaje_guardado' not in st.session_state:
+    st.session_state.puntaje_guardado = 0
 
-    session = requests.Session()
-    with st.form("my_form"):
-        index = st.text_input("Palabra clave", key="index")
+if 'puntaje_actual' not in st.session_state:
+    st.session_state.puntaje_actual = 0
 
-        submitted = st.form_submit_button("Buscar")
-
-        if submitted:
-            st.write("Result")
-            data = fetch(session, f"https://lexica.art/api/v1/search?q={index}")
-
-            data2 = data["images"][numero]
-            st.json = data2
-            st.json
-            if data2:
-                st.image(data2['src'], caption=f"Author: {data2['prompt']}")
-            else:
-                st.error("Error")
+if 'prompt_consultado' not in st.session_state:
+    st.session_state.prompt_consultado = "Prompt"
 
 
 # Parámetros
 
-numero_rand = random.randint(0, 49) # Para seleccionar aleatoreamente entre las 50 respuestas del json del servicio de Lexica
+# Para seleccionar aleatoreamente entre las 50 respuestas del json del servicio de Lexica
+numero_rand = random.randint(0, 49)
+puntaje_mayor = 0
 
-menu = ["Seleccione", "Modo temática texto", "Modo temática imagen","Modo aleatorio","Créditos"]
 
 st.set_page_config(
-	page_title="Adivina el prompt",
-	page_icon="random",
-	layout="centered",
-	initial_sidebar_state="expanded",
-	)
+    page_title="Adivina el prompt",
+    page_icon="random",
+    layout="centered",
+    initial_sidebar_state="expanded",
+)
 
-# Oculto botones de Streamlit
+# Parametros NLP
+#nlp = spacy.load("en_core_web_lg")
+nlp = spacy.load("en_core_web_sm")
+
+
+
+	# Oculto botones de Streamlit
 hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+				<style>
+				#MainMenu {visibility: hidden;}
+				footer {visibility: hidden;}
+				</style>
+				"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 
-#Funciones
+# Funciones
 def fetch(session, url):
     try:
         result = session.get(url)
@@ -58,67 +57,101 @@ def fetch(session, url):
         return {}
 
 
-def main():
-	st.title("Quest-the-prompt")
-
-	image =  PIL.Image.open('logo.png')
-
-	st.sidebar.image(image, use_column_width=False)
-	choice = st.sidebar.selectbox("Seleccione la opción en el menú",menu)
-
-	
-	if choice == "Seleccione":
-		st.subheader("Aplicación para entrenarse en el 'arte' de la redacción de prompts para la generación de imágenes con IA")
-
-	elif choice == "Modo temática texto":
-		st.subheader("Modo temática texto")
-		form = st.form("form1")
-		tematica_texto=form.text_input("Tema a buscar: ", key="tematica_texto")
-		texto_enviado = form.form_submit_button("Buscar")
-		
-		if texto_enviado:
-			session = requests.Session()
-			st.write("Resultado")
-			datos = fetch(session, f"https://lexica.art/api/v1/search?q={tematica_texto}")
-			datos_rand = datos["images"][numero_rand]
-			prompt_real=datos_rand['prompt']
-			URL_real=datos_rand['src']
-			#st.json = datos_rand
-			#st.json
-			if datos_rand:
-				#st.image(URL_real, caption=f"Prompt: {prompt_real}")
-				st.image(URL_real)
-			else:
-				st.error("Se ha producido un error. Espera 1 minuto para volver a intentar")
-			prompt_adivinado=form.text_input("Prompt adivinado: ", key="prompt_adivinado")
-			prompt_enviado = form.form_submit_button("Enviar")
-			if prompt_enviado:
-				st.write("Resultado")
-
-				#st.json = datos_rand
-				#st.json
+def consultar_imagen(tematica):
+    session = requests.Session()
+    datos = fetch(session, f"https://lexica.art/api/v1/search?q={tematica}")
+    datos_rand = datos["images"][numero_rand]
+    prompt_consultado = datos_rand['prompt']
+    URL_consultado = datos_rand['src']
+    return URL_consultado, prompt_consultado
 
 
+def adivinar_prompt(prompt_adivinado, prompt_real):
+    frase1 = prompt_adivinado
+    frase2 = prompt_real
+    fra1 = nlp(frase1)
+    fra2 = nlp(frase2)
+    similitud_frases = fra1.similarity(fra2)
+    puntaje_actual = similitud_frases
+    return puntaje_actual
 
 
-	elif choice == 'Modo temática imagen':
-		st.subheader("Modo temática imagen")
+# Logo sidebar
+image = PIL.Image.open('logo.png')
+st.sidebar.image(image, width=None, use_column_width=None)
+
+with st.sidebar:
+    selected = option_menu(
+        menu_title="Selecciona",  # required
+        options=["Home", "Modo random", "Modo tema"],  # required
+        icons=["house", "caret-right-fill",
+                        "caret-right-fill", "envelope"],  # optional
+        menu_icon="upc-scan",  # optional
+        default_index=0,  # optional
+    )
 
 
-	elif choice == 'Modo aleatorio':
-		st.subheader("Modo aleatorio")
+if selected == "Home":
+    st.title("Quest-the-prompt")
+    st.write("Esta aplicación te permitirá entrenarte en el arte de la redacción de prompts para la generación de imágenes por medio de IA.")
+    st.write("Selecciona la opción en el menú de la izquierda para iniciar")
+
+if selected == "Modo tema":
+	st.title(f"Adivinar el prompt / {selected}")
+	with st.form(key='form_tematica'):
+		tematica = st.text_input('Temática', "City")
+		boton_consultado = st.form_submit_button(
+			label="Generar imagen para adivinar", on_click=consultar_imagen, args=[tematica])
+		if boton_consultado:
+			URL_resultado = consultar_imagen(tematica)[0]
+			prompt_resultado = consultar_imagen(tematica)[1]
+			st.session_state.URL_consultado = URL_resultado
+			st.session_state.prompt_consultado = prompt_resultado
+			st.image(URL_resultado, caption='Imagen a adivinar')
+
+	with st.form(key='form_adivinar'):
+		prompt_real = st.session_state.prompt_consultado
+		prompt_adivinado = st.text_input('Prompt: ', " ")
+		boton_adivinar = st.form_submit_button("Adivinar", on_click=adivinar_prompt, args=[prompt_adivinado, prompt_real])
+
+		if boton_adivinar:
+			puntaje_actual = adivinar_prompt(prompt_adivinado, prompt_real)
+			st.session_state.puntaje_actual = puntaje_actual
+			puntaje_guardado = st.session_state.puntaje_guardado
+			diferencia = puntaje_actual - puntaje_guardado
+			if puntaje_actual > puntaje_guardado:
+				st.session_state.puntaje_guardado = puntaje_actual
+			col1, col2 = st.columns(2)
+			col1.metric(
+				label="Puntaje más alto: ⏳",
+				value=(st.session_state.puntaje_guardado),
+				delta=diferencia,
+			)
+			col2.metric(
+				label="Puntaje actual: ⏳",
+				value=(st.session_state.puntaje_actual),
+				delta=diferencia,
+			)
+
+			prompt_real = st.session_state.puntaje_actual
+			col1.write("**Prompt real**")
+			col1.write(st.session_state.prompt_consultado)
+			col2.write("**Prompt propuesto**")
+			col2.write(prompt_adivinado)
+			st.image(st.session_state.URL_consultado)
+
+if selected == "Modo random":
+	st.title(f"Adivina el prompt / {selected}")
 
 
 
 
-	elif choice == 'Créditos':
-		st.subheader("Jorge O. Cifuentes")
-		body='<a href="https://www.quidlab.co">https://www.quidlab.co</a>'
-		st.markdown(body, unsafe_allow_html=True)
-		st.write('Email: *jorge@quidlab.co* :heart: :fleur_de_lis:')
-		st.write("Quest-the-prompt")
-		st.write("Version 1.0")
-		st.text("")
-
-if __name__ == "__main__":
-	main()
+if selected == "Contacto":
+	st.title(f"Contacto y créditos")
+	st.subheader("Jorge O. Cifuentes")
+	body = '<a href="https://www.quidlab.co">https://www.quidlab.co</a>'
+	st.markdown(body, unsafe_allow_html=True)
+	st.write('Email: *jorge@quidlab.co* :heart: :fleur_de_lis:')
+	st.write("Quest-the-prompt")
+	st.write("Version 1.0")
+	st.text("")
